98287ece1e04a351070919900f27106c31897207
Symon Ogwe <symonogwe@gmail.com>
2024-03-13 14:44:34 +0300

    Create simple server that displays content on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const http = require("http");
+const path = require("path");
+const fs = require("fs/promises");
+
+const port = process.env.PORT || 5000;
+
+const myServer = http.createServer((req, res) => {
+  res.end("hello world my guy. this is your first ever node server!!");
+});
+
+myServer.listen(port, () => console.log("server is running"));
