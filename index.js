1bae03302ab4500c09ea201aab2f2940d09ecca3
Symon Ogwe <symonogwe@gmail.com>
2024-03-13 22:02:58 +0300

    Build server that serves content dynamically
    - Leveraged the core Node modules and built a server that serves the response dynamically
    based on the request from the user.

--- index.js
+++ index.js
@@ -5,7 +5,51 @@ const fs = require("fs/promises");
 const port = process.env.PORT || 5000;
 
 const myServer = http.createServer((req, res) => {
-  res.end("hello world my guy. this is your first ever node server!!");
+  // Get the file path based on the req.url
+  let filePath = path.join(
+    __dirname,
+    "Pages",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  // Get the path extension
+  const pathExtension = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (pathExtension) {
+    case ".js":
+      contentType = "application/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpeg":
+      contentType = "image/jpeg";
+      break;
+
+    default:
+      break;
+  }
+
+  async function readFiles() {
+    try {
+      const data = await fs.readFile(filePath, "utf8");
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    } catch (error) {
+      const errorPage = path.join(__dirname, "Pages", "404.html");
+      res.writeHead(404, { "Content-Type": contentType });
+      res.end(await fs.readFile(errorPage, "utf8"));
+    }
+  }
+
+  readFiles();
+
+  //   res.end();
 });
 
 myServer.listen(port, () => console.log("server is running"));
