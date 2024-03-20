2f33757d137c0a51b2574c1fb92952fb6c43dc30
Symon Ogwe <symonogwe@gmail.com>
2024-03-20 12:02:20 +0300

    Made the Pages folder static using Express

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+// Request Handlers
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "Pages", "index.html"));
+// });
+
+// app.get("/about.html", (req, res) => {
+//   res.sendFile(path.join(__dirname, "Pages", "about.html"));
+// });
+
+// app.get("/contact-me.html", (req, res) => {
+//   res.sendFile(path.join(__dirname, "Pages", "contact-me.html"));
+// });
+
+// Static Pages folder
+app.use(express.static(path.join(__dirname, "Pages")));
 
 app.listen(port, () => console.log("My Express server is running"));
