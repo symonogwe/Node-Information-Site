d4643f0a3b351680d4bcd97637de7c30546b1e9e
Symon Ogwe <symonogwe@gmail.com>
2024-03-20 13:45:52 +0300

    Add node_modules to gitignore

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Request Handlers
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "Pages", "index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Pages", "index.html"));
+});
 
-// app.get("/about.html", (req, res) => {
-//   res.sendFile(path.join(__dirname, "Pages", "about.html"));
-// });
+app.get("/about.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "Pages", "about.html"));
+});
 
-// app.get("/contact-me.html", (req, res) => {
-//   res.sendFile(path.join(__dirname, "Pages", "contact-me.html"));
-// });
+app.get("/contact-me.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "Pages", "contact-me.html"));
+});
 
 // Static Pages folder
-app.use(express.static(path.join(__dirname, "Pages")));
+// app.use(express.static(path.join(__dirname, "Pages")));
 
 app.listen(port, () => console.log("My Express server is running"));
